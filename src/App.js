da027a5c82a2cd72055e7f58ea05ddd5bd6d772b
salam <347849461@qq.com>
2020-06-08 21:30:36 +0800

    引入antd组件库

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, message } from 'antd';
 import './App.css';
 
 function App() {
+  function clickMe() {
+    message.info('点击了按钮');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary" onClick={clickMe}>
+          点我试试
+        </Button>
       </header>
     </div>
   );
